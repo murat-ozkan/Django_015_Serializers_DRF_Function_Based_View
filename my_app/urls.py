4f638e9f38b2acfb891f6f6d50442fdc4cae3f89
muratozkan79 <muratozkan79@gmail.com>
2023-05-23 01:34:25 +0300

    Model View Set

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,9 +1,39 @@
 from django.urls import path
-from .views import StudentListCreate, StudentDetailUpdateDelete
+from .views import (
+    StudentListCreate,
+    StudentDetailUpdateDelete,
+    # StudentGPPD,
+    StudentGenericListCreate,
+    StudentListCreateAPIView,
+    StudentRetrieveUpdateDestroyAPIView
+)
+
 
 urlpatterns = [
-    path("student_list_create/", StudentListCreate.as_view()), # 1
-    path("student_detail_update_delete/<int:pk>", StudentDetailUpdateDelete.as_view()),
+    path('student_list_create/', StudentListCreate.as_view()),
+    path('student_detail_update_delete/<int:pk>', StudentDetailUpdateDelete.as_view()),
+    # path('student_gppd/', StudentGPPD.as_view()),
+    # path('student_gppd/<int:pk>', StudentGPPD.as_view()),
+    path('student_generic_list_create/', StudentGenericListCreate.as_view()),
+    path('student_list_create_api/', StudentListCreateAPIView.as_view()),
+    path('student_get_put_delete_api/<int:pk>', StudentRetrieveUpdateDestroyAPIView.as_view()),
 ]
 
 # 1 Bir URL çağırıldığında bir fonksiyon çalışır. Ancak bu bir fonk değil. Bir method. Class içindeki fonksiyonlar bir metottur.
+
+
+
+# ----------------------------------------------------------------
+# Router for ModelViewSet
+
+from .views import StudentMVS
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+# Register the path
+router.register('students', StudentMVS) # URL sonunda / yok. # router.register('another', AnotherMVS)
+
+# Add to paths:
+urlpatterns += router.urls
+
--- my_app/views.py
+++ my_app/views.py
@@ -60,7 +60,6 @@ class StudentDetailUpdateDelete(APIView):
 # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 # ----------------------------------------------------------------
 # Yukarıdaki işlemleri tek class'da yazmaya çalışalım
 
@@ -101,3 +100,75 @@ class StudentDetailUpdateDelete(APIView):
 #         student = get_object_or_404(Student, id=pk)
 #         student.delete()
 #         return Response({ "message": "Deleted" }, status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+# ----------------------------------------------------------------
+# GenericAPIView
+# https://www.django-rest-framework.org/api-guide/generic-views/#genericapiview
+# Mixins
+# https://www.django-rest-framework.org/api-guide/generic-views/#mixins
+# ListCreateAPIView
+# RetrieveUpdateDestroyAPIView
+# https://www.django-rest-framework.org/api-guide/generic-views/#concrete-view-classes
+# ----------------------------------------------------------------
+
+
+from rest_framework.generics import GenericAPIView
+from rest_framework import mixins
+class StudentGenericListCreate(mixins.ListModelMixin, mixins.CreateModelMixin, GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+# Bu işlemler için bir yol daha var.
+
+# ----------------------------------------------------------------
+# ListCreateAPIView
+
+# RetrieveUpdateDestroyAPIView
+
+# https://www.django-rest-framework.org/api-guide/generic-views/#concrete-view-classes
+# ----------------------------------------------------------------
+
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+class StudentListCreateAPIView(ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    # lookup_field = "id" # Default: "pk" olarak ayarlı. Değiştirilebilir bu şekilde.
+
+
+
+# ----------------------------------------------------------------
+# ModelViewSet:
+
+# https://www.django-rest-framework.org/api-guide/viewsets/#modelviewset
+
+# ----------------------------------------------------------------
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+
+# Tüm İşlemleri yapar.....
+class StudentMVS(ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    
+    
+    # özel veri istendiğinde, aşağıdaki decorator eklenebilir.
+    # Mesela öğrenci sayısı
+    @action(methods=["GET"], detail=False)
+    def count(self, request):
+        return Response({
+            "count": Student.objects.count()
+        })
