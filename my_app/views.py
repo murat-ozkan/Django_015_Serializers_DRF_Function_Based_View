c156c9e2a603e086a64066d4357b2b190a1bd9da
muratozkan79 <muratozkan79@gmail.com>
2023-05-23 00:28:52 +0300

    up

--- my_app/views.py
+++ my_app/views.py
@@ -55,3 +55,49 @@ class StudentDetailUpdateDelete(APIView):
                 'message': 'Instance deleted successfully'
             }, status = status.HTTP_202_ACCEPTED)
 
+# Alternative Responses:
+# return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+# return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+# ----------------------------------------------------------------
+# Yukarıdaki işlemleri tek class'da yazmaya çalışalım
+
+# class StudentGPPD(APIView):
+
+#     def get(self, request, pk=0):
+#         if pk:
+#         # Tek kayıt görüntüle:
+#             student = get_object_or_404(Student, id=pk)
+#             serializer = StudentSerializer(instance=student)
+#             return Response(serializer.data)
+#         else:
+#         # Kayıtları listele:
+#             students = Student.objects.all()
+#             serializer = StudentSerializer(instance=students, many=True)
+#             return Response(serializer.data)
+    
+#     # Yeni Kayıt (POST Method)
+#     def post(self, request):
+#         serializer = StudentSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     # Tek kayıt güncelle:
+#     def put(self, request, pk):
+#         student = get_object_or_404(Student, id=pk)
+#         serializer = StudentSerializer(instance=student, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+#     # Tek kayıt sil:
+#     def delete(self, request, pk):
+#         student = get_object_or_404(Student, id=pk)
+#         student.delete()
+#         return Response({ "message": "Deleted" }, status=status.HTTP_204_NO_CONTENT)
